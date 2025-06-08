106758cdec7ebf335758a60bb241e5a32989f693
Laurens Martin <laurensmbesselsen@gmail.com>
2025-06-08 12:15:29 +0200

    Attempt to fix image caching

--- src/config.ts
+++ src/config.ts
@@ -1,12 +1,8 @@
 import {NOTION_API_TOKEN} from '$env/static/private'
-import path from 'path'
 
 export default {
   notion: {
     token:       NOTION_API_TOKEN,
     databaseID: '1dcd99b727fb80aa9e81c5b59feb0b5b',
-  },
-  images: {
-    cacheDirectory: path.join(process.cwd(), 'src', 'static', 'image-cache')
   },
 }
