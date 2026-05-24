3c84b84f4c7efa4c802b2387a49cb02d97b709d3
Laurens Martin <laurensmbesselsen@gmail.com>
2026-05-24 21:14:43 +0200

    Use dynamic env variables

--- src/config.ts
+++ src/config.ts
@@ -1,8 +1,8 @@
-import {NOTION_API_TOKEN} from '$env/static/private'
+import {env} from '$env/dynamic/private'
 
 export default {
   notion: {
-    token:       NOTION_API_TOKEN,
+    token:       env.NOTION_API_TOKEN,
     databaseID: '1dcd99b727fb80aa9e81c5b59feb0b5b',
   },
 }
